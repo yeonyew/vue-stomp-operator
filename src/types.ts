20188398165a815af9b475200c78386e0817e25e
Jichan <development@jc-lab.net>
2019-12-09 10:59:35 +0900

    update

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,27 @@
-import {Client as StompClient} from "@stomp/stompjs";
+import {
+    Client as StompClient, IMessage
+} from "@stomp/stompjs";
 
 export type errorCallback = (err: any) => void;
 export type stompErrorCallback = (err: any, stomp: StompClient) => void;
 export type connectCallback = () => void;
 export type closeCallback = () => void;
+
+export interface IResponseEvent {
+    raw: IMessage;
+    status: number;
+    headers: Record<string, string>;
+    body: any;
+}
+
+export type responseInterceptorCallback = (responseEvent: IResponseEvent) => Promise<IResponseEvent> | IResponseEvent;
+export type validateStatusInterceptorCallback = (responseEvent: IResponseEvent) => Promise<boolean> | boolean;
+
+export interface IInterceptors {
+    response?: responseInterceptorCallback;
+    validateStatus?: validateStatusInterceptorCallback;
+}
+
+export interface ISendOptions extends IInterceptors {
+    timeout?: number;
+}
